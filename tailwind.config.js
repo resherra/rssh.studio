c77a1160d839069ba5a338cb3a9c89d052ce16e5
jack <Ciao@lazych.me>
2022-10-13 22:20:18 +0100

    rebuild#131022

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,24 @@
 module.exports = {
     content: ["./public/**/*.{html,js}"],
     theme: {
+      colors: {
+        transparent: 'transparent',
+        current: 'currentColor',
+        'mainColor': '#121212',
+        'textColor': '#f1f1f1',
+        'gridsColor': '#282828'
+
+      },
+      clipPath: {
+        mypolygon: "polygon(0% 7%, 100% 0%, 100% 100%, 0% 100%)",
+    },
       fontFamily: {
         'sans': ['montserrat', 'Segoe UI', 'Roboto']
           },
       extend: {},
     },
-    plugins: [],
+
+    plugins: [
+      require('tailwind-clip-path'),
+    ],
 }
