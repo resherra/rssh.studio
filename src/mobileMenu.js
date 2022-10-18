f8f56ed6bbab3fa23fb80aa66a5956cc0a13baac
jack <Ciao@lazych.me>
2022-10-18 18:04:07 +0100

    rebuild

---
+++ src/mobileMenu.js
@@ -0,0 +1,18 @@
+// class MobileMenu {
+//     constructor () {
+//         this.button = document.querySelector(`.toggle-button`)
+//         this.menuList = document.getElementById(`menu-list`)
+//         this.event()
+//     }
+
+//     event() {
+//         this.button.addEventListener(`click`, () => this.toggleTheMenu())
+//     }
+
+//     toggleTheMenu() {
+//         console.log(`hello jack!`)
+//         this.menuList.classList.toggle('hidden')
+//     }
+// }
+
+// export default MobileMenu
