7a809c6b4ca6a58e9fa340cb605137589ae3ef35
jack <Ciao@lazych.me>
2022-10-18 18:03:34 +0100

    rebuild

--- src/script.js
+++ src/script.js
@@ -2,10 +2,31 @@ function serve(target, url) {
     var r = new XMLHttpRequest();
     r.open("GET", url, true);
     r.onreadystatechange = function () {
-        if (r.readyState != 3 || r.status != 200) return;
+        if (r.readyState != 4 || r.status != 200) return;
         target.innerHTML = r.responseText;
 
-        // toggle-button                
+        // toggle-button          
+        
+        class MobileMenu {
+            constructor () {
+                this.button = document.querySelector(`.toggle-button`)
+                this.menuList = document.getElementById(`menu-list`)
+                this.event()
+            }
+        
+            event() {
+                this.button.addEventListener(`click`, () => this.toggleTheMenu())
+            }
+        
+            toggleTheMenu() {
+                console.log(`hello jack!`)
+                this.menuList.classList.remove(`hidden`)
+            }
+        }
+
+        
+        let red = new MobileMenu()
+
     };
     r.send();
 
