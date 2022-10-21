dfa5814d9c01dd13749fcb1b91cb975e295112bf
jack <Ciao@lazych.me>
2022-10-21 01:51:56 +0100

    rebuild#211022

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,4 @@
-const currentTask = process.env.npm_lifecycle_event
 const path = require('path')
-const MiniCssExtractPlugin = require("mini-css-extract-plugin")
-
 
 const config = {
     entry: [`./src/app.js`],
@@ -25,13 +22,6 @@ const config = {
             }
         ]
     }
-}
-
-
-if (currentTask == "build") {
-    config.mode = "production"
-    config.module.rules[0].use[0] = MiniCssExtractPlugin.loader
-    config.plugins.push(new MiniCssExtractPlugin({ filename: "main.css" }))
 }
 
 module.exports = config
