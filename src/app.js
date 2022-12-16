d7181edfe35a551644dcabf9f9c68da73edb6ab1
jack <Ciao@lazych.me>
2022-12-16 21:36:21 +0100

    adding comments

--- src/app.js
+++ src/app.js
@@ -7,7 +7,11 @@ import wait from "waait";
 //serve content.
 let includes = Array.from(document.querySelectorAll('[data-include]'));
 includes.map(include => {
+    // include = <div data-include="views/nav"></div>
     let file = include.dataset['include'] + '.html';
+    //file = views/nav.html
+    //serve(target ,url)
+    // server(<div data-include="views/nav"></div>, views/nav.html)
     serve(include, file);
 });
 
