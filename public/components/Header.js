57e7c97850b125bf2bb34663b55d55ee6db265d3
jack <Ciao@lazych.me>
2022-12-29 14:41:41 +0100

    nav menu

--- public/components/Header.js
+++ public/components/Header.js
@@ -1,8 +1,10 @@
-import React from "react"
+import React, { useState } from "react"
 import { Link } from "react-router-dom"
 import Page from "./Page"
 
 function Header() {
+  const [show, setShow] = useState(false)
+
   return (
     <Page>
       <nav className="max-w-screen-lg p-4 lg:m-auto flex items-baseline justify-between">
@@ -14,25 +16,19 @@ function Header() {
 
         <div>
           <div>
-            <ul className="flex-col lg:flex lg:flex-row gap-5 lg:gap-7 pl-4 lg:pl-0 text-center lg:mt-10 bg-zinc-200 bg-opacity-5 backdrop-blur-[1.2px] lg:bg-transparent fixed lg:static top-0 lg:-top-0 bottom-0 lg:-bottom-0 z-10 lg:z-auto pt-[5rem] lg:pt-0 right-0 lg:-right-0 w-screen lg:w-full hidden" id="menu-list">
+            <ul onClick={() => setShow(!show)} className={`flex-col lg:flex lg:flex-row gap-5 lg:gap-7 pl-4 lg:pl-0 text-center lg:mt-10 bg-zinc-200 bg-opacity-5 backdrop-blur-[1.2px] lg:bg-transparent fixed lg:static top-0 lg:-top-0 bottom-0 lg:-bottom-0 z-10 lg:z-auto pt-[5rem] lg:pt-0 right-0 lg:-right-0 w-screen lg:w-full + ${show ? " flex" : " hidden"}`}>
               <li>
-                <Link to="/About" className="nav-link">
-                  About
-                </Link>
+                <Link to="/About">About</Link>
               </li>
               <li>
-                <Link to="/Blog" className="nav-link">
-                  Blog
-                </Link>
+                <Link to="/Blog">Blog</Link>
               </li>
               <li>
-                <Link to="/Contact" className="nav-link">
-                  Contact
-                </Link>
+                <Link to="/Contact">Contact</Link>
               </li>
             </ul>
 
-            <button onClick={handleNav} className="toggle-button w-5 cursor-pointer relative lg:hidden z-20" aria-label="toggle">
+            <button onClick={() => setShow(!show)} className="toggle-button w-5 cursor-pointer relative lg:hidden z-20" aria-label="toggle">
               <span className="block bg-textColor h-0.5 mb-0.5 rounded-lg"></span>
               <span className="block bg-textColor h-0.5 mb-0.5 rounded-lg"></span>
               <span className="block bg-textColor h-0.5 rounded-lg"></span>
@@ -42,10 +38,6 @@ function Header() {
       </nav>
     </Page>
   )
-
-  function handleNav(e) {
-    console.log("test!")
-  }
 }
 
 export default Header
--- public/components/Home.js
+++ public/components/Home.js
@@ -1,7 +1,14 @@
-import React from "react"
+import React, { useState } from "react"
+import HomeServices from "./HomeServices"
 import Page from "./Page"
 
 function Home() {
+  const [active, setActive] = useState(false)
+
+  function handleClick() {
+    setActive(!active)
+  }
+
   return (
     <Page title="Home">
       <div className="h-[32rem] max-w-screen-lg p-4 lg:m-auto">
@@ -10,14 +17,14 @@ function Home() {
           <p className="patterns">
             I help my clients to
             <span className="bg-clip-text text-transparent bg-gradient-to-l from-gradientTo to-gradientFrom">
-              <span data-services="" data-link="link1" className="font-semibold cursor-pointer select-none">
+              <span onClick={handleClick} className="font-semibold cursor-pointer select-none">
                 {" "}
                 Ideate.
               </span>{" "}
-              <span data-services="" data-link="link2" className="font-semibold cursor-pointer select-none">
+              <span onClick={handleClick} className="font-semibold cursor-pointer select-none">
                 Prototype.
               </span>{" "}
-              <span data-services="" data-link="link3" className="font-semibold cursor-pointer select-none">
+              <span onClick={handleClick} className="font-semibold cursor-pointer select-none">
                 Build.
               </span>
             </span>
@@ -25,30 +32,7 @@ function Home() {
           </p>
         </div>
       </div>
-      <div className="bg-gridsColor rounded-b-3xl lg:rounded-none w-screen-lg lg:m-auto clip-path-mypolygon lg:clip-path-none">
-        <div className="grid grid-rows-3 lg:grid-cols-3 lg:grid-rows-1 gap-5 lg:gap-10 pt-20 pb-10 lg:py-14 max-w-screen-lg p-4 lg:m-auto">
-          <div className="bg-mainColor rounded-[2.5rem] p-8 flex flex-col justify-center lg:justify-start drop-shadow-md">
-            <h3 id="services" data-link="link1" className="font-semibold pb-6 lg:pb-10 lg:pt-3">
-              Ideate
-            </h3>
-            <p className="lg:pb-10">I am here to help you consider the product's architecture, design system, tech stack, and features..</p>
-          </div>
-
-          <div className="bg-mainColor rounded-[2.5rem] p-8 flex flex-col justify-center lg:justify-start drop-shadow-md">
-            <h3 id="services" data-link="link2" className="font-semibold pb-6 lg:pb-10 lg:pt-3">
-              Prototype
-            </h3>
-            <p className="lg:pb-10">Get your MVP ready and share it with your earliest adopters to gather feedback.</p>
-          </div>
-
-          <div className="bg-mainColor rounded-[2.5rem] p-8 flex flex-col justify-center lg:justify-start drop-shadow-md">
-            <h3 id="services" data-link="link3" className="font-semibold pb-6 lg:pb-10 lg:pt-3">
-              Build & deploy
-            </h3>
-            <p className="lg:pb-10">I will work to enhance your product and elevate it to the next level, then distribute the final version to the intended audience.</p>
-          </div>
-        </div>
-      </div>
+      <HomeServices active={active} />
     </Page>
   )
 }
