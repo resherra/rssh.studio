37314d37cb364fcc9b728fc3203d33b88e18aaf4
jack <Ciao@lazych.me>
2023-01-03 23:56:30 +0100

    Fixes

--- public/components/Footer.js
+++ public/components/Footer.js
@@ -9,13 +9,13 @@ function Footer() {
         <div className="flex flex-col items-center gap-3">
           <h3 className="font-semibold">Find me on:</h3>
           <div className="flex gap-4">
-            <a href="https://twitter.com/lazycherrat" target="_blank">
+            <a aria-label="Twitter link" href="https://twitter.com/lazycherrat" target="_blank">
               <img className="w-5" src="./images/twitter-icon.svg" alt="twitter" width="640" height="360" />
             </a>
-            <a href="https://www.linkedin.com/in/redouan-cherrat-b951b81bb/" target="_blank">
+            <a aria-label="Linkedin link" href="https://www.linkedin.com/in/redouan-cherrat-b951b81bb/" target="_blank">
               <img className="w-5" src="./images/linkedin-icon.svg" alt="linkedin" width="640" height="360" />
             </a>
-            <a href="https://github.com/lazych" target="_blank">
+            <a aria-label="Github link" href="https://github.com/lazych" target="_blank">
               <img className="w-5" src="./images/github-icon.svg" alt="github" width="640" height="360" />
             </a>
           </div>
@@ -33,7 +33,7 @@ function Footer() {
           </Link>
         </div>
 
-        <div className="flex gap-2 pb-4">
+        <div aria-label="Copyright" className="flex gap-2 pb-4">
           <img className="w-3" src="./images/copyright-icon.svg" alt="" width="640" height="360" />
           <p>lazych {new Date().getFullYear()}</p>
         </div>
--- public/components/Home.js
+++ public/components/Home.js
@@ -3,6 +3,25 @@ import HomeServices from "./HomeServices"
 import Page from "./Page"
 
 function Home() {
+  const [test, setTest] = useState([
+    { id: 1, bool: false },
+    { id: 2, bool: false },
+    { id: 3, bool: false },
+  ])
+
+  function handleClick(e) {
+    const mapArround = test.map((item) => {
+      if (item.bool) {
+        item.bool = false
+      }
+      if (item.id.toString() === e.target.id) {
+        return { ...item, bool: !item.bool }
+      }
+      return item
+    })
+    return setTest(mapArround)
+  }
+
   return (
     <Page title="Home">
       <div className="h-[32rem] max-w-screen-lg p-4 lg:m-auto">
@@ -11,13 +30,22 @@ function Home() {
           <p className="patterns">
             I help my clients to
             <span className="bg-clip-text text-transparent bg-gradient-to-l from-gradientTo to-gradientFrom">
-              <span className="font-semibold cursor-pointer select-none"> Ideate.</span> <span className="font-semibold cursor-pointer select-none">Prototype.</span> <span className="font-semibold cursor-pointer select-none">Build. </span>
+              <span id="1" onClick={handleClick} className="font-semibold cursor-pointer select-none">
+                {" "}
+                Ideate.
+              </span>{" "}
+              <span id="2" onClick={handleClick} className="font-semibold cursor-pointer select-none">
+                Prototype.
+              </span>{" "}
+              <span id="3" onClick={handleClick} className="font-semibold cursor-pointer select-none">
+                Build.{" "}
+              </span>
             </span>
             seamless & User-friendly experiences.
           </p>
         </div>
       </div>
-      <HomeServices />
+      <HomeServices test={test} />
     </Page>
   )
 }
