2f2dcf2d3563133f69e1f799d30d2eedfbba6d06
lazych <redouancherrat@gmail.com>
2023-04-23 16:47:44 +0000

    /src

--- app/blog/page.js
+++
@@ -1,7 +0,0 @@
-export default function BlogPage() {
-  return (
-    <div className={`min-h-[32rem] font-sans text-3xl flex items-center justify-center py-24`}>
-      <p>Sorry, there&apos;s no blog post yet! Submit your email below to stay updated.</p>
-    </div>
-  )
-}
