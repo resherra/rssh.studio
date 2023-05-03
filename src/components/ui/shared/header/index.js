354146e7f1060ecce10c62ad005457becd93c6c5
lazych <redouancherrat@gmail.com>
2023-05-03 16:32:35 +0100

    fixing header

--- src/components/ui/shared/header/index.js
+++ src/components/ui/shared/header/index.js
@@ -25,7 +25,7 @@ export default function Header() {
   return (
     <div className={`pb-28 font-mono text-sm`}>
       <nav className={`w-full z-20 fixed top-0 right-0 ${scroll && `backdrop-blur-md md:backdrop-blur-sm bg-mainColor dark:bg-black/50 border-b border-secColor dark:border-mainGray `}`}>
-        <div className={`p-4 md:p-8 md:pb-2`}>
+        <div className={`p-4 md:px-8 md:pt-6 md:pb-2`}>
           <div className={`flex flex-row-reverse justify-between items-center`}>
             <Links />
             <Switcher />
