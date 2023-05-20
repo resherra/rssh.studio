4b12abd41ec7e2c8bbd3fd20ade1bdb7056c4aeb
lazych <redouancherrat@gmail.com>
2023-05-20 01:59:12 +0100

    sitemap

---
+++ src/app/sitemap.js
@@ -0,0 +1,20 @@
+export default function sitemap() {
+  return [
+    {
+      url: "https://chred.me",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://chred.me/about",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://chred.me/tools",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://chred.me/blog",
+      lastModified: new Date(),
+    },
+  ]
+}
